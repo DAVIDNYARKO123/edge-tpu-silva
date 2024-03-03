d2ff34f09895a0663d9c3c4d7a590549ebc3fbd6
DAVIDNYARKO123 <nyarkprince6@gmail..com>
2024-03-02 21:49:54 -0500

    v1.0.4

--- example/object_detect.py
+++ example/object_detect.py
@@ -3,7 +3,10 @@ from edge_tpu_silva import process_detection
 # Example Usage with Required Parameters
 model_path = 'path/to/your/model.tflite'
 input_path = 'path/to/your/input/video.mp4'
-labels_path = 'path/to/your/labels.txt'
+imgsz = 240
 
 # Run the object detection process
-process_detection(model_path, input_path, labels_path)
+outs = process_detection(model_path, input_path, imgsz)
+
+for _, _, _ in outs:
+  pass
